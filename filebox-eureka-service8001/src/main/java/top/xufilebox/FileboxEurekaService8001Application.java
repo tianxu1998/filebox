c9a17232891ee28672d55b61004972917e51852c
Tianxu <2655541751@qq.com>
2020-12-10 19:40:21 +0800

    first commit

---
+++ filebox-eureka-service8001/src/main/java/top/xufilebox/FileboxEurekaService8001Application.java
@@ -0,0 +1,13 @@
+package top.xufilebox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileboxEurekaService8001Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FileboxEurekaService8001Application.class, args);
+    }
+
+}
---
+++ filebox-eureka-service8002/src/test/java/top/xufilebox/FileboxEurekaService8002ApplicationTests.java
@@ -0,0 +1,13 @@
+package top.xufilebox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileboxEurekaService8002ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
