6e994c988679a0d4ed6257fa4a1dc7cff0991f41
Tianxu <2655541751@qq.com>
2020-12-12 19:47:09 +0800

    修改配置文件夹名称为filebox-config

--- filebox-config-service8011/src/main/java/top/xufilebox/FileboxConfigService8011Application.java
+++ filebox-config-service8011/src/main/java/top/xufilebox/FileboxConfigService8011Application.java
@@ -2,8 +2,12 @@ package top.xufilebox;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
+@EnableConfigServer
 public class FileboxConfigService8011Application {
 
     public static void main(String[] args) {
--- filebox-eureka-service8001/src/main/java/top/xufilebox/FileboxEurekaService8001Application.java
+++ filebox-eureka-service8001/src/main/java/top/xufilebox/FileboxEurekaService8001Application.java
@@ -4,9 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-import java.util.Arrays;
-import java.util.Scanner;
-
 @SpringBootApplication
 @EnableEurekaServer
 public class FileboxEurekaService8001Application {
